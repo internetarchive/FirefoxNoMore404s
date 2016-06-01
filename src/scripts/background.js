7877110989eca4ef15cc6c3a92399bed21ddcd7b
Peter deHaan <peter@deseloper.com>
2016-05-31 17:05:34 -0700

    Add .eslintrc file and tweak some errors

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,3 +1,5 @@
+/* global chrome */
+
 /**
  * Header callback
  */
@@ -9,18 +11,18 @@ chrome.webRequest.onCompleted.addListener(function(details) {
     if (isIncognito === false &&
         details.statusCode === 404 &&
         details.frameId === 0 &&
-        !details.url.startsWith('http://web.archive.org/web/') &&
-        !details.url.startsWith('https://web.archive.org/web/')) {
+        !details.url.startsWith("http://web.archive.org/web/") &&
+        !details.url.startsWith("https://web.archive.org/web/")) {
       wmAvailabilityCheck(details.url, function(response, url) {
         // inject response into page
         chrome.tabs.executeScript(details.tabId, {
-          code: 'window.__WAYBACK_PAGE_URL = ' + JSON.stringify(url),
+          code: "window.__WAYBACK_PAGE_URL = " + JSON.stringify(url)
         });
         chrome.tabs.executeScript(details.tabId, {
-          code: 'window.__WAYBACK_RESPONSE = ' + JSON.stringify(response),
+          code: "window.__WAYBACK_RESPONSE = " + JSON.stringify(response)
         });
         chrome.tabs.executeScript(details.tabId, {
-          file: "scripts/client.js",
+          file: "scripts/client.js"
         });
       });
     }
@@ -28,28 +30,28 @@ chrome.webRequest.onCompleted.addListener(function(details) {
   chrome.tabs.get(details.tabId, function(tab) {
     tabIsReady(tab.incognito);
   });
-}, {urls: ['<all_urls>'], types: ['main_frame']});
+}, {urls: ["<all_urls>"], types: ["main_frame"]});
 
 /**
  * borrowed from Adam's ChromeNoMore404s plugin
  */
 function wmAvailabilityCheck(url, onsuccess, onfail, onerror) {
   var xhr = new XMLHttpRequest();
-  var requestUrl = 'https://archive.org/wayback/available?url=' + url;
-  xhr.open('GET', requestUrl, true);
-  xhr.setRequestHeader('User-Agent', navigator.userAgent + ' FirefoxNoMore404s/1.2.0');
+  var requestUrl = "https://archive.org/wayback/available?url=" + url;
+  xhr.open("GET", requestUrl, true);
+  xhr.setRequestHeader("User-Agent", navigator.userAgent + " FirefoxNoMore404s/1.2.0");
   xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4) {
-      if (xhr.status == 200) {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
         var response = JSON.parse(xhr.responseText);
         if (response.archived_snapshots &&
             response.archived_snapshots.closest &&
             response.archived_snapshots.closest.available &&
             response.archived_snapshots.closest.available === true &&
-            response.archived_snapshots.closest.status.indexOf('2') === 0) {
+            response.archived_snapshots.closest.status.indexOf("2") === 0) {
           onsuccess(response, url);
         } else if (onfail && response.archived_snapshots &&
-            !response.archived_snapshots.hasOwnProperty('closest')) {
+            !response.archived_snapshots.hasOwnProperty("closest")) {
           onfail(response, url);
         }
       } else {
